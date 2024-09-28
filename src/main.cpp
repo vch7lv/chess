accdf52fd63c43688a11317bfa21b4ed5aac5322
slavick7878 <vyacheslavosipov03@gmail.com>
2024-09-28 12:19:07 +0300

    delete useless files)))))

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,6 @@
-#include <SFML/Graphics.hpp>
-#include "Callback.h"
 #include <iostream>
-
-struct sum
-{
-    void operator () (int a, int b, int c) const
-    {
-        std::cout << a+b+c << std::endl;
-    }
-};
-
-void f (int a, int b, int c) {std::cout << a << ' ' << b << ' ' << c << std::endl;}
 
 int main()
 {
-    auto p = createCallback(&f, 1, 2, 3);
-    p->invoke();
-}git
+
+}
